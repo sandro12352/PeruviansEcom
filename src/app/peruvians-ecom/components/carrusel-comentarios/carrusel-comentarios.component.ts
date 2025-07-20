37de41daef0a0085e50d5894f1e29b501e4fcedc
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2025-07-19 23:53:59 -0500

    cambios en comentarios

--- src/app/peruvians-ecom/components/carrusel-comentarios/carrusel-comentarios.component.ts
+++ src/app/peruvians-ecom/components/carrusel-comentarios/carrusel-comentarios.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'peruvians-carrusel-comentarios',
   templateUrl: './carrusel-comentarios.component.html',
   styleUrl: './carrusel-comentarios.component.css'
 })
-export class CarruselComentariosComponent {
+export class CarruselComentariosComponent implements OnInit{
+  
+    public ocultarUltimoSlide: boolean = false;
+  
+  
+  ngOnInit(): void {
+    if (window.innerWidth >= 768) {
+      this.ocultarUltimoSlide = true;
+    }
+  }
+    
+
+
 
 }
