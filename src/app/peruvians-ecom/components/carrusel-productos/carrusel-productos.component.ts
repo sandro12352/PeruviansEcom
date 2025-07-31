0dba67e0f61be72a1089ace481b67bd511cf78fc
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2025-07-30 23:31:35 -0500

    agregando scroll con dedo

--- src/app/peruvians-ecom/components/carrusel-productos/carrusel-productos.component.ts
+++ src/app/peruvians-ecom/components/carrusel-productos/carrusel-productos.component.ts
@@ -1,56 +1,22 @@
-import { AfterViewInit, Component, HostListener, Inject, Input, OnChanges, PLATFORM_ID } from '@angular/core';
+import { Component, HostListener, Inject, Input, OnChanges, PLATFORM_ID } from '@angular/core';
 import { Producto } from '../../interfaces/producto';
 import { isPlatformBrowser } from '@angular/common';
-declare var bootstrap: any;
+
 
 @Component({
   selector: 'peruvians-carrusel-productos',
   templateUrl: './carrusel-productos.component.html',
   styleUrl: './carrusel-productos.component.css'
 })
-export class CarruselProductosComponent implements OnChanges,AfterViewInit  {
+export class CarruselProductosComponent implements OnChanges {
   @Input() carouselId!: string;
   @Input() public productos!: Producto[];
-  gruposProductos: Producto[][] = [];
-
-  private touchStartX = 0;
-  private touchEndX = 0;
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object
   ){}
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      const carousel = document.getElementById(this.carouselId);
-      if (carousel) {
-        carousel.addEventListener('touchstart', (e: TouchEvent) => {
-          this.touchStartX = e.changedTouches[0].screenX;
-        });
 
-        carousel.addEventListener('touchend', (e: TouchEvent) => {
-          this.touchEndX = e.changedTouches[0].screenX;
-          this.handleSwipe(carousel);
-        });
-      }
-    }
-  }
-
-  private handleSwipe(carouselElement: HTMLElement) {
-    const deltaX = this.touchEndX - this.touchStartX;
-
-    if (Math.abs(deltaX) > 50) {
-      const instance = bootstrap.Carousel.getInstance(carouselElement);
-      if (instance) {
-        if (deltaX > 0) {
-          instance.prev(); // swipe derecha
-        } else {
-          instance.next(); // swipe izquierda
-        }
-      }
-    }
-  }
-
-  
+  gruposProductos: Producto[][] = [];
 
   ngOnChanges () {
     this.calcularGrupos();
