961e3ddfbd550745acfe2f6fda78a16db4c3b992
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2025-07-31 00:09:01 -0500

    agregando tocar con el dedo en movil

--- src/app/peruvians-ecom/components/carrusel-productos/carrusel-productos.component.ts
+++ src/app/peruvians-ecom/components/carrusel-productos/carrusel-productos.component.ts
@@ -1,57 +1,64 @@
-import { Component, HostListener, Inject, Input, OnChanges, PLATFORM_ID } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  HostListener,
+  Inject,
+  Input,
+  OnChanges,
+  PLATFORM_ID
+} from '@angular/core';
 import { Producto } from '../../interfaces/producto';
 import { isPlatformBrowser } from '@angular/common';
 
+declare var bootstrap: any;
 
 @Component({
   selector: 'peruvians-carrusel-productos',
   templateUrl: './carrusel-productos.component.html',
   styleUrl: './carrusel-productos.component.css'
 })
-export class CarruselProductosComponent implements OnChanges {
+export class CarruselProductosComponent implements OnChanges, AfterViewChecked {
   @Input() carouselId!: string;
-  @Input() public productos!: Producto[];
+  @Input() productos!: Producto[];
 
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object
-  ){}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   gruposProductos: Producto[][] = [];
+  carouselInstanciado = false;
 
-  ngOnChanges () {
+  ngOnChanges() {
     this.calcularGrupos();
+    this.carouselInstanciado = false; // para reinicializar el carrusel
   }
 
   @HostListener('window:resize')
   onResize() {
     this.calcularGrupos();
+    this.carouselInstanciado = false; // reinicia si cambia de tamaño
   }
-  
 
-
- calcularGrupos() {
-  if (isPlatformBrowser(this.platformId)) {
-    const ancho = window.innerWidth;
-    const chunkSize = ancho < 768 ? 2 : 5;
-
-    this.gruposProductos = [];
-    for (let i = 0; i < this.productos.length; i += chunkSize) {
-      this.gruposProductos.push(this.productos.slice(i, i + chunkSize));
+  ngAfterViewChecked(): void {
+    if (!this.carouselInstanciado && isPlatformBrowser(this.platformId)) {
+      const el = document.getElementById(this.carouselId);
+      if (el) {
+        new bootstrap.Carousel(el, {
+          touch: true,
+          interval: false
+        });
+        this.carouselInstanciado = true;
+      }
     }
   }
-}
-  
- 
 
+  calcularGrupos() {
+    if (isPlatformBrowser(this.platformId)) {
+      const ancho = window.innerWidth;
+      const chunkSize = ancho < 768 ? 2 : 5;
 
- 
-
-
-
-
-  
-
-
-
-
+      this.gruposProductos = [];
+      for (let i = 0; i < this.productos.length; i += chunkSize) {
+        this.gruposProductos.push(this.productos.slice(i, i + chunkSize));
+      }
+    }
+  }
 }
